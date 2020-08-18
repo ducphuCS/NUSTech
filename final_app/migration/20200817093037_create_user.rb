dc6e61892fa07243fbdf703bab20042c209f2bf7
ducphuCS <phu.nguyen.cse@hcmut.edu.vn>
2020-08-18 15:33:06 +0700

    move final_app

--- final_app/migration/20200817093037_create_user.rb
+++
@@ -1,6 +0,0 @@
-class CreateUser < ActiveRecord::Migration[6.0]
-  def change
-    create_table :users do |t|
-    end
-  end
-end
--- final_app/migration/20200817094619_add_email_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddEmailToUser < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :email, :string, limit:500
-  end
-end

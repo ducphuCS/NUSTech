0488a54766556efe37ffd0753018844d0eba9b8d
ducphuCS <phu.nguyen.cse@hcmut.edu.vn>
2020-08-19 16:45:21 +0700

    active record validation exercise

--- finalapp/app/models/photo.rb
+++ finalapp/app/models/photo.rb
@@ -1,2 +1,7 @@
 class Photo < ApplicationRecord
+  validates :title, presence: true
+  belongs_to :users, touch: true
+  after_touch do
+    puts 'An Photo was touched'
+  end
 end
--- finalapp/app/models/user.rb
+++ finalapp/app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ApplicationRecord
+  has_many :photos
+  validates :email, presence: true, format: /@/, uniqueness: true
+  validates :password, presence: true, length: { in: 6..20}
+  validates :firstname, uniqueness: {scope: :lastname}
+  validates_each :firstname, :lastname do |record, attr, value|
+    record.errors.add(attr, "must start with upper case") if value =~ /\A[[:lower:]]/
+  end
+  with_options if: :admin? do |admin|
+    admin.validates :password, length: {minimum: 10}
+  end
+  after_touch :log_when_photos_or_users_touched
+  private
+    def log_when_photos_or_users_touched
+      puts "Photo/User was touched"
+    end
 end

537de53a33b9d67bac9aec2e1a5c53645f21da0f
ducphuCS <phu.nguyen.cse@hcmut.edu.vn>
2020-08-19 16:44:57 +0700

    update table Person

--- get_started_with_rails_exercise/blog/app/models/person.rb
+++ get_started_with_rails_exercise/blog/app/models/person.rb
@@ -1,3 +1,15 @@
+class GoodnessValidator < ActiveModel::Validator
+  def validate(record)
+    if record.name == "Evil"
+      record.errors[:base] << "This person is evil"
+    end
+  end
+end
+
 class Person < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: {strict: true}
+  validates_with GoodnessValidator
+  validates_each :name, :surname do |record, attr, value|
+    record.errors.add(attr, "must start with upper case") if value =~ /\A[[:lower:]]/
+  end
 end

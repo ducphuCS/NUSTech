d4477052eeed0c9e0e9e3447e5ab206db5b142fc
ducphuCS <phu.nguyen.cse@hcmut.edu.vn>
2020-08-28 14:01:14 +0700

    create Home Controller

---
+++ finalapp/app/controllers/home_controller.rb
@@ -0,0 +1,9 @@
+class HomeController < ApplicationController
+  def index
+    redirect_to action: :newest
+  end
+  def newest
+    @albums = Album.order(created_at: :desc).limit(4).select(:id, :title)
+    render "newest"
+  end
+end

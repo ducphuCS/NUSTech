76ae67a3ef746404bf5becfba5dc5dc1209654b2
ducphuCS <phu.nguyen.cse@hcmut.edu.vn>
2020-08-30 23:04:43 +0700

    install jquery and config
    https://stackoverflow.com/questions/62097010/rails-6-and-jquery-plugin-error-validate-is-not-a-function

--- finalapp/config/webpack/environment.js
+++ finalapp/config/webpack/environment.js
@@ -1,3 +1,22 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+
+environment.plugins.append('ProvidePlugin-jQuery',
+  new webpack.ProvidePlugin({
+    jQuery: 'jquery',
+    jquery: 'jquery',
+    Popper: ['popper.js', 'default']
+  })
+)
+
+environment.loaders.append("jquery", {
+  test: require.resolve("jquery"),
+  use: [
+    {
+      loader: "expose-loader",
+      options: { exposes: ["$", "jQuery"] }
+    },
+  ],
+});
 
 module.exports = environment

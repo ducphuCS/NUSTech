eb20015fdfd9ad4c73c2946af0f0df12ed086034
ducphuCS <phu.nguyen.cse@hcmut.edu.vn>
2020-09-03 17:03:58 +0700

    update view

--- finalapp/app/javascript/packs/script.js
+++ finalapp/app/javascript/packs/script.js
@@ -6,6 +6,8 @@ $(document).ready(function() {
       if (!btnPhoto.hasClass("chosen")) {
         btnPhoto.addClass("chosen")
         btnAlbum.removeClass("chosen")
+        btnPhoto.removeClass("bg-white")
+        btnAlbum.addClass("bg-white")
       }
     }
   })
@@ -14,6 +16,8 @@ $(document).ready(function() {
       if (!btnAlbum.hasClass("chosen")) {
         btnAlbum.addClass("chosen")
         btnPhoto.removeClass("chosen")
+        btnAlbum.removeClass("bg-white")
+        btnPhoto.addClass("bg-white")
       }
     }
   })
@@ -40,19 +44,54 @@ $(document).ready(function() {
         $("#modal-photo-title").html(title);
         $("#modal-photo-body").attr("src", source)
         $("#modal-photo-description").html(description);
-        $("#modal-photo").toggle();
+        $("#modal-photo").modal('toggle')
       }
       else {
         $("#modal-album-title").html(title)
         $("#modal-album-description").html(description)
         // this is for source of picture in album
-        $("#modal-album").toggle();
+        $("#modal-album").modal('toggle')
       }
     }
   })
   $(".photo-modal-close").on( {
     "click": function() {
       $("#modal-photo").toggle();
+    }
+  })
+  $(".album-modal-close").on( {
+    "click": function() {
+      $("#modal-album").toggle();
+    }
+  })
+  $(".carousel-control-prev").on( {
+    "click": function() {
+      var inner = $(".carousel-inner")
+      var noCarousel = inner.find('.carousel-item').length;
+      var active = inner.find('.carousel-item.active')
+      if (active.is(':first-child')) {
+        active.removeClass("active")
+        inner.children().last().addClass("active")
+      }
+      else {
+        active.prev().addClass("active")
+        active.removeClass("active")
+      }
+    }
+  })
+  $(".carousel-control-next").on( {
+    "click": function() {
+      var inner = $(".carousel-inner")
+      var noCarousel = inner.find('.carousel-item').length;
+      var active = inner.find('.carousel-item.active')
+      if (active.is(':last-child')) {
+        active.removeClass("active")
+        inner.children().first().addClass("active")
+      }
+      else {
+        active.next().addClass("active")
+        active.removeClass("active")
+      }
     }
   })
   $("button[name='follow']").on({
